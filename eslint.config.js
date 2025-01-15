1fcb965662d4ba1d605970a5dbd2becae0e12460
Yoann Canal <yoann.canal@gmail.com>
2025-01-15 01:11:43 +0100

    Initial Commit

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js'
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import reactCompiler from 'eslint-plugin-react-compiler'
+import importPlugin from 'eslint-plugin-import';
+
+export default tseslint.config(
+    { ignores: ['dist'] },
+    {
+        extends: [
+            js.configs.recommended,
+            importPlugin.flatConfigs.recommended,
+            ...tseslint.configs.recommended
+        ],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'react-compiler': reactCompiler,
+            "simple-import-sort": simpleImportSort,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            'react-compiler/react-compiler': "error",
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+        },
+    },
+)
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react-swc'
+// @ts-expect-error - no TS types yet for beta test.
+import reactCompiler from 'babel-plugin-react-compiler'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        reactCompiler,
+        react(),
+        tsconfigPaths()
+    ],
+})
+
