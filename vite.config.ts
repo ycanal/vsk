57b3f3bebd4d18a17db4f4ea64d294604290b1c3
Yoann Canal <yoann.canal@gmail.com>
2025-02-05 00:32:28 +0100

    chore: update to tailwind 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 // @ts-expect-error - no TS types yet for beta test.
 import reactCompiler from "babel-plugin-react-compiler";
@@ -6,5 +7,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactCompiler, react(), tsconfigPaths()],
+  plugins: [reactCompiler, react(), tailwindcss(), tsconfigPaths()],
 });
